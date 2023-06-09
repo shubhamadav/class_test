d7768186aeb9651ea3388ecd89216b40d5ee4005
shubhamadav <shubhamyad100@gmail.com>
2023-06-10 00:31:20 +0530

    I will change the 2 iteam color and 3 iteam invisbal

--- dem1.js
+++ dem1.js
@@ -1,11 +1,19 @@
 //console.dir(document);
 
-console.log(items);
-var items = document.getElementsByClassName("list-group-item");
-if (items.length > 0) {
-  items[0].textContent = "Hello";
-  items[2].style.backgroundColor = 'green';
-}
+//console.log(items);
+//var items = document.getElementsByClassName("list-group-item");
+//if (items.length > 0) {
+  //items[0].textContent = "Hello";
+  //items[2].style.backgroundColor = 'green';
+//}
+const secondItem = document.querySelector('#items li:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+
+const thirdItem = document.querySelector('#items li:nth-child(3)');
+thirdItem.style.visibility = 'hidden';
+
+
 
 
 
