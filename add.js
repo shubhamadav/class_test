a61b607e51a9111b6782af5a8d9a1c74fd8ee0fa
shubhamadav <shubhamyad100@gmail.com>
2023-06-26 00:44:45 +0530

    Wathapp update

--- add.js
+++ add.js
@@ -1,75 +1,51 @@
-function addDetails() {
-    var name = document.getElementById("nameInput").value;
-    var email = document.getElementById("emailInput").value;
-    var category = document.getElementById("choosecatagory").value;
-    var details = {
-      name: name,
-      email: email,
-      category:category
-    };
-    
-    
-    var storedDetails = localStorage.getItem("details");
-    var detailsList = [];
-    
-    if (storedDetails) {
-      detailsList = JSON.parse(storedDetails);
-    }
-    
-    detailsList.push(details);
-    localStorage.setItem("details", JSON.stringify(detailsList));
-    
-    
-    document.getElementById("nameInput").value = "";
-    document.getElementById("emailInput").value = "";
-    document.getElementById("choosecatagory").value = "";
-   
-    displayDetails();
-  }
-  
- 
-  function displayDetails() {
-    var storedDetails = localStorage.getItem("details");
-    var detailsList = [];
-    
-    if (storedDetails) {
-      detailsList = JSON.parse(storedDetails);
-    }
-    
-    var detailsListElement = document.getElementById("detailsList");
-    detailsListElement.innerHTML = "";
-    
-    for (var i = 0; i < detailsList.length; i++) {
-      var details = detailsList[i];
-      
-      var listItem = document.createElement("li");
-      listItem.innerHTML = "<strong>Name:</strong> " + details.name + ", <strong>Email:</strong> " + details.email +"<strong>Category:</strong> " + details.category;
-      
-      var deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      deleteButton.setAttribute("data-index", i);
-      deleteButton.onclick = deleteDetails;
-      
-      listItem.appendChild(deleteButton);
-      detailsListElement.appendChild(listItem);
-    }
-  }
-  
-  
-  function deleteDetails() {
-    var index = this.getAttribute("data-index");
-    var storedDetails = localStorage.getItem("details");
-    var detailsList = [];
-    
-    if (storedDetails) {
-      detailsList = JSON.parse(storedDetails);
-    }
-    
-    detailsList.splice(index, 1);
-    localStorage.setItem("details", JSON.stringify(detailsList));
-    
-    displayDetails();
-  }
-  
-  // Initial display
-  displayDetails();
+function updateLastUserActivityTime() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const lastActivityTime = new Date().toLocaleTimeString();
+      resolve(lastActivityTime);
+    }, 1000);
+  });
+}
+
+// Example usage:
+const posts = [];
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      resolve(posts);
+    }, 2000);
+  });
+}
+
+function deleteLastPost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length > 0) {
+        posts.pop();
+        resolve(posts);
+      } else {
+        reject(new Error("No posts to delete."));
+      }
+    }, 1500);
+  });
+}
+
+// Usage example:
+createPost("First post")
+  .then((newPosts) => {
+    console.log("Posts created:", newPosts);
+    return updateLastUserActivityTime();
+  })
+  .then((lastActivityTime) => {
+    console.log("Last activity time:", lastActivityTime);
+    return deleteLastPost();
+  })
+  .then((updatedPosts) => {
+    console.log("Posts after deletion:", updatedPosts);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
